3bcafa3aca23e838b1f5d73ff890d916a6140837
Andrew Gonzales <andrewmartingonzales@gmail.com>
2015-10-05 17:24:27 -0700

    (doc) Adds comments to main game over screen

--- public/client/js/gameover.js
+++ public/client/js/gameover.js
@@ -1,8 +1,16 @@
+/*****************************
+ Game over big screen view
+*****************************/
+  //This is the view displayed on the main screen after a round is over 
+  //It is triggered from game.js after a winner is declared.
+  //After an eight second pause, the game will restart on the main lobby screen
+  //Listens for: none
+  //Emits: 'reset game'
+
 var gameOver = function(game){};
  
 gameOver.prototype = {
   init: function(){
-    // alert("Winner: ", winner);
   },
 
   preLoad: function(){
@@ -11,20 +19,20 @@ gameOver.prototype = {
 
   },
   create: function(){
+
+    //Add images to home screen
+
     var gameOverTitle = this.game.add.sprite(game.world.width * (1/2), game.world.height * (1/3),'gameover');
     gameOverTitle.anchor.setTo(0.5,0.5);
-    var playButton = this.game.add.button(game.world.width * (1/2), game.world.height * (2/3),"play",this.playTheGame,this);
+    //This button is currently inactive. The change from the "lobby" state to the "main" game state is triggered 
+    //from the server as described above.
+    var playButton = this.game.add.button(game.world.width * (1/2), game.world.height * (2/3),"play",null,this);
     playButton.anchor.setTo(0.5,0.5);
 
+    //Change view back to lobby screen after 8 seconds
     setTimeout(function(){
       socket.emit("reset game");
       game.state.start("Lobby",true,false);
     },8000);
-  },
-
-
-  playTheGame: function(){
-
-    this.game.state.start("Lobby", true, false);
   }
 };
